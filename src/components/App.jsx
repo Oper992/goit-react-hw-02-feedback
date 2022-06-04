8661e43b263a835e65c0f3a841b4b499fce795c5
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2022-06-04 22:33:47 +0300

    задание 1 почти готово

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions"
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addState = evt => {
+    const buttonName = evt.target.name;
+
+    this.setState(prevState => {
+      if (buttonName === 'good') {
+        return { good: prevState.good + 1 };
+      }
+      if (buttonName === 'neutral') {
+        return { neutral: prevState.neutral + 1 };
+      } else {
+        return { bad: prevState.bad + 1 };
+      }
+    });
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const total = this.countTotalFeedback();
+
+    return Math.round((this.state.good / total) * 100);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <button type="button" name="good" onClick={this.addState}>
+          Good
+        </button>
+        <button type="button" name="neutral" onClick={this.addState}>
+          Neutral
+        </button>
+        <button type="button" name="bad" onClick={this.addState}>
+          Bad
+        </button>
+        <FeedbackOptions options={} onLeaveFeedback={}/>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+export default function Statistics({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total}</li>
+        <li>
+          Positive feedback: {positivePercentage ? positivePercentage : '0'}%
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
